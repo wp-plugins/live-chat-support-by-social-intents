83b72078e4791b93c28d9ccdccd1fb1028cd4063
socialintents <socialintents@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-07 14:46:04 +0000

    1.0.5
    git-svn-id: https://plugins.svn.wordpress.org/live-chat-support-by-social-intents/tags/1.0.5@1021544 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- live-chat-support-by-socialintents.php
+++ live-chat-support-by-socialintents.php
@@ -107,6 +107,8 @@ http://www.socialintents.com/" title="', '">', '</a>') ?></p>
             </div>
 	    <div id="silc_registerComplete" class="inside" style="padding: -20px 10px;display:none;">
 		<p>View reports, customize chat widget and CSS styles, and export chat history on our website at <a href='http://www.socialintents.com'>www.socialintents.com</a>
+		</p>
+		<p>Answer Chats from your Agent Dashboard: <a href='https://www.socialintents.com/chat.do'>Agent Dashboard</a>
 		</p><form id='saveDetailSettings' method="post" action="options.php">
 		<?php wp_nonce_field('update-options') ?>
 		<input type="hidden" name="action" value="update" />
@@ -352,7 +354,7 @@ $.ajax({
     success: function(json) {
 	if (json.msg=='') {
          	$('#silc_widgetID').val(json.key);
-		alert("Thanks for signing up!  Now Customize your settings...");
+		alert("Thanks for signing up!  You can use these same credentials to login to socialintents.com.  Now Customize your settings...");
 		$( "#saveSettings" ).submit();
 		
 	}
